6cb408384a184fab130faaac9cb4b59f2cae2279
sahindudilshan <109513920+sahindudilshan@users.noreply.github.com>
2022-07-18 20:20:16 +0530

    Update Config.js

--- Config.js
+++ Config.js
@@ -16,26 +16,26 @@ global.APIKeys = {
 global.emoji01 = ['🐼'] // 👈 Enter your Emoji 
 global.emoji02 = ['🦋'] //👈 Enter your Emoji
 global.notice = ` *Hi 🥰 I m*
-*ＮＯＩＺ Ξ*
-*I am 16 years old developer...*
+*〲ꜱʜᴀɴ⃜│⦁⃝〽️⃯⃖❤️ᷤ*
+*I am 18 years old developer...*
 *From Sri Lanka 🇱🇰*
 *I am a Learning.*
 *Not more..*
 *Also I am Student*
-*Follow My Facebook - https://www.facebook.com/zenoixnoize*`  // 👈 Enter Your details and Social Media Links and Anything
-global.owner =  ['+94711502119'] //👈  Enter Your number
-global.premium =  ['+94711502119'] //👈  Enter Your number
-global.ownernomer = '+94711502119' //👈  Enter Your number
-global.ownername = 'ZENOI' //👈 Enter Your name
-global.botname = 'ＧＡＲＦＩＥＬＤ ＢＯＴ' //👈 Enter Your Bot name
-global.footer = '© 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒 𝖦𝖺𝗋𝖿𝗂𝖾𝗅𝖽 𝖡𝗈𝗍' //👈 Enter Your Name
-global.ig = 'https://github.com/Zenoixnoize/GARFIELD-WHATSAPP-BOT-v8' // 👈 You Can change this your choice
-global.region = 'India, South India, Kerala' // 👈 You Can change this your choice
+*Join My whatsapp group - https://chat.whatsapp.com/LvBi39qA9eSEG66OzQR768*`  // 👈 Enter Your details and Social Media Links and Anything
+global.owner =  ['+94704227434'] //👈  Enter Your number
+global.premium =  ['+94704227434'] //👈  Enter Your number
+global.ownernomer = '+94704227434' //👈  Enter Your number
+global.ownername = '〲ꜱʜᴀɴ⃜│⦁⃝〽️⃯⃖❤️ᷤ' //👈 Enter Your name
+global.botname = '〲ꜱʜᴀɴ⃜│⦁⃝〽️⃯⃖❤️ᷤ ＢＯＴ' //👈 Enter Your Bot name
+global.footer = '© 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒 〲ꜱʜᴀɴ⃜│⦁⃝〽️⃯⃖❤️ᷤ 𝖡𝗈𝗍' //👈 Enter Your Name
+global.ig = 'https://github.com/〲ꜱʜᴀɴ⃜│⦁⃝〽️⃯⃖❤️ᷤ/GARFIELD-WHATSAPP-BOT-v8' // 👈 You Can change this your choice
+global.region = 'Sri Lankan,Buddhist' // 👈 You Can change this your choice
 global.sc = 'https://github.com/Zenoixnoize/GARFIELD-WHATSAPP-BOT-v8'
 global.fbt = 'Follw Us ℹ️' // 👈 Enter (Subscribe/Follow/Like now) 
 global.myweb = 'https://www.facebook.com/garfieldbots/' // 👈 Enter your Social media link to follow now button
-global.packname = 'GARFIELD-BOTv8'  // 👈 You Can change this your choice 
-global.author = 'NOIZE DEVELOPMENT' // 👈 You Can change this your choice 
+global.packname = '〲ꜱʜᴀɴ⃜│⦁⃝〽️⃯⃖❤️ᷤ'  // 👈 You Can change this your choice 
+global.author = '〲ꜱʜᴀɴ⃜│⦁⃝〽️⃯⃖❤️ᷤ DEVELOPMENT' // 👈 You Can change this your choice 
 global.sessionName = 'session' // 👈 You Can change this your choice
 global.prefa = ['','!','.','🛰️','⚓','🍎'] // 👈 You Can change this your choice
 //Go to https://imgbb.com/, upload a photo of your choice and put its link here
